6f974d2e49ba558d7321c48e5d0357ff0a98a373
Marc-André Moreau <marcandre.moreau@gmail.com>
2018-12-05 11:07:53 -0500

    Initial Rust CLI (#1)
    * albatross-rs: start automating sysroot creation
    
    * albatross-rs: add basic toolchain installation
    
    * albatross-rs: minor cleanup
    
    * albatross-rs: update toolchain files
    
    * README: update

---
+++ src/config.rs
@@ -0,0 +1,64 @@
+
+use std::env;
+use std::path::{PathBuf};
+
+#[derive(Clone)]
+pub struct Config {
+    pub albatross_home: PathBuf,
+    pub toolchain_dir: PathBuf,
+    pub sysroot_dir: PathBuf,
+    pub cmake_dir: PathBuf,
+    pub tmp_dir: PathBuf,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Config {
+            albatross_home: PathBuf::new(),
+            toolchain_dir: PathBuf::new(),
+            sysroot_dir: PathBuf::new(),
+            cmake_dir: PathBuf::new(),
+            tmp_dir: PathBuf::new(),
+        }
+    }
+
+    pub fn load_default(&mut self) {
+
+        let home_env = if cfg!(target_os = "windows") {
+            "USERPROFILE"
+        } else {
+            "HOME"
+        };
+
+        let home_dir = env::var(home_env).unwrap();
+
+        self.albatross_home = PathBuf::from(home_dir).join(".albatross");
+    }
+
+    pub fn load_env(&mut self) {
+        if let Ok(val) = env::var("ALBATROSS_HOME") {
+            self.albatross_home = PathBuf::from(Some(val).unwrap());
+        }
+    }
+
+    pub fn setup(&mut self) {
+        self.toolchain_dir = self.albatross_home.clone().join("toolchain");
+        self.sysroot_dir = self.albatross_home.clone().join("sysroot");
+        self.cmake_dir = self.albatross_home.clone().join("cmake");
+        self.tmp_dir = self.albatross_home.clone().join("tmp");
+
+        fs_extra::dir::create_all(&self.albatross_home, false).unwrap();
+        fs_extra::dir::create_all(&self.toolchain_dir, false).unwrap();
+        fs_extra::dir::create_all(&self.sysroot_dir, false).unwrap();
+        fs_extra::dir::create_all(&self.cmake_dir, false).unwrap();
+        fs_extra::dir::create_all(&self.tmp_dir, false).unwrap();
+    }
+
+    pub fn load() -> Self {
+        let mut config = Config::new();
+        config.load_default();
+        config.load_env();
+        config.setup();
+        config
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+#[macro_use]
+extern crate clap;
+
+extern crate fs_extra;
+
+use std::path::{PathBuf};
+
+use std::env;
+use std::path::Path;
+use std::error::Error;
+
+use std::process::Command;
+
+use clap::App;
+
+mod config;
+use config::Config;
+
+fn run_toolchain(_input: &str, config: &Config) {
+    env::set_current_dir(config.tmp_dir.clone()).unwrap();
+
+    let llvm_host_os = if cfg!(target_os = "linux") {
+        "linux-gnu-ubuntu-16.04"
+    } else if cfg!(target_os = "macos") {
+        "apple-darwin"
+    } else {
+        "unknown"
+    };
+
+    let llvm_base_url = "https://releases.llvm.org";
+    let llvm_version = "7.0.0";
+    let llvm_host_arch = "x86_64";
+    let llvm_basename = format!("clang+llvm-{}-{}-{}", llvm_version, llvm_host_arch, llvm_host_os);
+    let llvm_filename = format!("{}.{}", llvm_basename, "tar.xz");
+
+    let download_url = format!("{}/{}/{}",
+        llvm_base_url, llvm_version, llvm_filename);
+
+    if !PathBuf::from(llvm_filename.clone()).exists() {
+        println!("downloading {}", llvm_filename);
+        let _ = Command::new("wget")
+            .arg(download_url)
+            .output()
+            .expect("failed to download clang+llvm");
+    }
+
+    if !PathBuf::from(llvm_basename.clone()).exists() {
+        println!("extracting {}", llvm_filename);
+        let _ = Command::new("tar")
+            .arg("-xf")
+            .arg(llvm_filename)
+            .output()
+            .expect("failed to download clang+llvm");
+    }
+
+    let src_file = format!("{}", llvm_basename);
+    let dst_file = format!("{}/llvm", config.toolchain_dir.display());
+
+    let mut options = fs_extra::dir::CopyOptions::new();
+    options.overwrite = true;
+    options.copy_inside = true;
+    options.content_only = true;
+
+    fs_extra::dir::create_all(dst_file.clone(),false).unwrap();
+    fs_extra::dir::copy(src_file, dst_file, &options).unwrap();
+}
+
+fn run_sysroot(input: &str, config: &Config) {
+    env::set_current_dir(Path::new(input)).unwrap();
+
+    let cwd = env::current_dir().unwrap();
+    let sysroot_name = cwd.file_name().unwrap().to_str().unwrap();
+
+    let docker_v = format!("{}/{}:/sysroot", config.sysroot_dir.display(), sysroot_name);
+    let docker_c = "cd /sysroot && cp -R -L /lib lib && cp -R -L /usr usr";
+
+    let _ = Command::new("docker")
+        .arg("run")
+        .arg("-v")
+        .arg(docker_v)
+        .arg(sysroot_name)
+        .arg("/bin/bash")
+        .arg("-c")
+        .arg(docker_c)
+        .output()
+        .expect("failed to execute docker");
+
+    let src_file = "toolchain.cmake";
+    let dst_file = format!("{}/{}.cmake", config.cmake_dir.display(), sysroot_name);
+
+    let _ = Command::new("cp")
+        .arg(src_file)
+        .arg(dst_file)
+        .output()
+        .expect("failed to install cmake toolchain file");
+
+}
+
+fn run(matches: &clap::ArgMatches, config: &Config) -> Result<(), Box<Error>> {
+    match matches.subcommand() {
+        ("toolchain", Some(_args)) => {
+            run_toolchain("install", &config);
+            Ok(())
+        },
+        ("sysroot", Some(args)) => {
+            let input = args.value_of("input").unwrap();
+            run_sysroot(input, &config);
+            Ok(())
+        },
+        _ => { Err("unrecognized subcommand")? },
+    }
+}
+
+fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let app = App::from_yaml(yaml);
+    let config = Config::load();
+    let matches = app.version(crate_version!()).get_matches();
+
+    match run(&matches, &config) {
+        Ok(()) => { return }
+        Err(e) => { println!("{}", e)}
+    }
+}
